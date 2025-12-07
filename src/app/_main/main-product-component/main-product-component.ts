6ddc674471cc55f6d4c88f252b6190e8de819bd5
Furkan Kahraman <mfurkankahraman@gmail.com>
2025-12-07 20:53:43 +0300

    feat: implement dynamic category filtering in main product component

--- src/app/_main/main-product-component/main-product-component.ts
+++ src/app/_main/main-product-component/main-product-component.ts
@@ -1,6 +1,8 @@
+import { CategoryService } from './../../_services/categoryService';
 import { Product } from '../../_models/product';
 import { ProductService } from './../../_services/product-service';
 import { Component } from '@angular/core';
+import { Category } from '../../_models/category';
 
 @Component({
   selector: 'app-main-product-component',
@@ -10,11 +12,14 @@ import { Component } from '@angular/core';
 })
 export class MainProductComponent {
   productList: Product[] = [];
+  categoryList: Category[] = [];
   /**
    *
    */
-  constructor(private ProductService: ProductService) {
+  constructor(private ProductService: ProductService,
+    private CategoryService: CategoryService) {
     this.getAllProducts();
+    this.getCategories();
   }
 
   getAllProducts() {
@@ -22,4 +27,13 @@ export class MainProductComponent {
       next: values => this.productList = values
     })
   }
+
+  getCategories() {
+    this.CategoryService.getAll().subscribe({
+      next: values => this.categoryList = values
+    })
+  }
+
+
+
 }
--- src/app/_models/category.ts
+++ src/app/_models/category.ts
@@ -1,4 +1,7 @@
+import { Product } from "./product";
+
 export class Category{
   id;
   categoryName;
+  products: Product[];
 }
